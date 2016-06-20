9d2b52210be2e8967b53914fc340131d1b8cda18
jeffthemaximum <frey.maxim@gmail.com>
2016-06-20 18:57:20 -0400

    search form looks like a form

--- rails_frontend/app/controllers/grants_controller.rb
+++ rails_frontend/app/controllers/grants_controller.rb
@@ -1,5 +1,9 @@
+require 'grant_search.rb'
+
 class GrantsController < ApplicationController
-    def index
-        @grants = Grant.all
-    end
+  def index
+
+    # @grants = Grant.all
+    @grant_search_form = GrantSearchForm.new
+  end
 end
