c23061aad8d472d8b22cb240c0fa4b58ee0869f4
jeffthemaximum <frey.maxim@gmail.com>
2016-06-20 18:11:48 -0400

    pg search with jsonb support

--- rails_frontend/app/models/grant.rb
+++ rails_frontend/app/models/grant.rb
@@ -1,3 +1,15 @@
 class Grant < ActiveRecord::Base
+    include PgSearch
     self.table_name = "grants_grant"
+    multisearchable :against => [:search_description, :search_link]
+
+    pg_search_scope :pg_search_description, :against => PgSearch::Configuration::JsonbColumn.new(:data, 'description')
+
+    def search_description
+      data['description']
+    end
+
+    def search_link
+      data['link']
+    end
 end
