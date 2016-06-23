62fe4d09968cd420be3fe29a9686c00c60da6680
jeffthemaximum <frey.maxim@gmail.com>
2016-06-23 19:03:17 -0400

    organization

--- django-backend/grants/models.py
+++ django-backend/grants/models.py
@@ -14,6 +14,7 @@ class Funder(models.Model):
 # Create your models here.
 class Grant(models.Model):
     funder = models.ForeignKey(Funder, null=True, blank=True, default=None)
+    organization = models.CharField(max_length=500, null=True, blank=True, default=None)
     data = JSONField()
     created_at = models.DateTimeField(editable=False)
     updated_at = models.DateTimeField(null=True, blank=True, default=None)
